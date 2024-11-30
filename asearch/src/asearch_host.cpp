13eab5ddf079e25466f88f2ff9ae35fa354f23af
Matthew Schlittler <m.schlittler@wustl.edu>
2024-11-30 13:23:48 -0600

    First attempt at making host code work

--- asearch/src/asearch_host.cpp
+++ asearch/src/asearch_host.cpp
@@ -21,96 +21,107 @@
 #include <fstream>
 #include <iostream>
 #include <stdlib.h> // for system()
+#include <string>
 
-// XRT includes
+ // XRT includes
 #include "experimental/xrt_bo.h"
 #include "experimental/xrt_device.h"
 #include "experimental/xrt_kernel.h"
 
 #define DATA_SIZE 4096
 
-int main(int argc, char **argv) {
-  // const int SAMPLES=600;
-  const int SAMPLES = 20;
+bool cmpLine(const string& str1, const string& str2);
 
-  // Command Line Parser
-  sda::utils::CmdLineParser parser;
+int main(int argc, char** argv)
+{
+    // Command Line Parser
+    sda::utils::CmdLineParser parser;
 
-  // Switches
-  //**************//"<Full Arg>",  "<Short Arg>", "<Description>", "<Default>"
-  parser.addSwitch("--xclbin_file", "-x", "input binary file string", "");
-  parser.addSwitch("--device_id", "-d", "device index", "0");
-  parser.parse(argc, argv);
+    // Switches
+    //**************//"<Full Arg>",  "<Short Arg>", "<Description>", "<Default>"
+    parser.addSwitch("--xclbin_file", "-x", "input binary file string", "");
+    parser.addSwitch("--device_id", "-d", "device index", "0");
+    parser.parse(argc, argv);
 
-  // Read settings
-  std::string binaryFile = parser.value("xclbin_file");
-  int device_index = stoi(parser.value("device_id"));
+    // Read settings
+    std::string binaryFile = parser.value("xclbin_file");
+    int device_index = stoi(parser.value("device_id"));
 
-  if (argc < 3) {
-    parser.printHelp();
-    return EXIT_FAILURE;
-  }
-
-  std::cout << "Open the device" << device_index << std::endl;
-  auto device = xrt::device(device_index);
-  std::cout << "Load the xclbin " << binaryFile << std::endl;
-  auto uuid = device.load_xclbin(binaryFile);
-
-  auto krnl = xrt::kernel(device, uuid, "asearch");
-
-  std::cout << "Allocate Small Buffer For Input in Device Memory\n";
-  auto bo_out = xrt::bo(device, 128, krnl.group_id(0));
-
-  // Map the contents of the buffer object into host memory
-  auto bo_out_map = bo_out.map<int *>();
-
-  std::ifstream file_in;
-  std::ofstream file_out;
-  file_in.open("input.dat");
-  file_out.open("out.dat");
-
-/*
-  data_t signal;
-  signal = 0;
-  std::cout << "Execution of the kernel\n";
-  for (int i = 0; i < SAMPLES; ++i) {
-    file_in >> signal;
-    std::cout << "Iteration " << i << std::endl;
-    auto run = krnl(bo_out, signal);
-    run.wait();
-    bo_out.sync(XCL_BO_SYNC_BO_FROM_DEVICE);
-    file_out << bo_out_map[0] << std::endl;
-  }
-
-  file_in.close();
-  file_out.close();
-
-  // Comparing results with the golden output.
-  printf("Comparing observed against expected data \n");
-
-  std::ifstream file_obs, file_exp;
-  file_obs.open("out.dat");
-  file_exp.open("out.gold.fir11.dat");
-
-  data_t observed, expected;
-  for (int i = 0; i < 20; ++i) {
-    file_obs >> observed;
-    file_exp >> expected;
-    // start at 11 to account for garbage data before shift reg is fully
-    // populated
-    if (i > 10) {
-      if (observed != expected) {
-        std::cout << "*******************************************" << std::endl;
-        std::cout << "FAIL: Output DOES NOT match the golden output"
-                  << std::endl;
-        std::cout << "*******************************************" << std::endl;
-        return 1;
-      }
+    if (argc < 3)
+    {
+        parser.printHelp();
+        return EXIT_FAILURE;
     }
-  }
-  std::cout << "*******************************************" << std::endl;
-  std::cout << "PASS: The output matches the golden output" << std::endl;
-  std::cout << "*******************************************" << std::endl;
-*/
-  return 0;
+
+    std::cout << "Open the device" << device_index << std::endl;
+    auto device = xrt::device(device_index);
+    std::cout << "Load the xclbin " << binaryFile << std::endl;
+    auto uuid = device.load_xclbin(binaryFile);
+
+    auto krnl = xrt::kernel(device, uuid, "asearch");
+
+    std::cout << "Allocate Small Buffer For Input in Device Memory\n";
+    auto bo_out = xrt::bo(device, 128, krnl.group_id(0));
+
+    // Map the contents of the buffer object into host memory
+    auto bo_out_map = bo_out.map<int*>();
+
+    int grid[COL][ROW];
+    Pair src = make_pair(8, 0);
+    Pair dest = make_pair(0, 0);
+    result r = result::PATH_NOT_FOUND;
+    cell details[ROW][COL];
+    readGrid("input.dat", grid);
+
+    std::cout << "Execution of the kernel\n";
+    auto run = krnl(grid, src, dest, &r, details);
+    run.wait();
+
+
+    // Comparing results with the golden output.
+    printf("Comparing observed against expected data \n");
+
+    std::ifstream file_obs, file_exp;
+    file_obs.open("out.dat");
+    file_exp.open("out.gold.aStarSearch.dat");
+    std::string observed, expected;
+    bool hasDataObs, hasDataExp;
+    do
+    {
+        hasDataObs = std::getline(file_obs, observed);
+        hasDataExp = std::getline(file_exp, expected);
+
+        if (hasDataObs != hasDataExp || // Make sure both agree if there should be more data
+            (hasDataExp && hasDataObs && !cmpLine(observed, expected))) // If both have data but don't match
+        {
+            std::cout << "*******************************************" << std::endl;
+            std::cout << "FAIL: Output DOES NOT match the golden output" << std::endl;
+            std::cout << "*******************************************" << std::endl;
+            return 1;
+        }
+    } while (hasDataObs && hasDataExp);
+
+    std::cout << "*******************************************" << std::endl;
+    std::cout << "PASS: The output matches the golden output" << std::endl;
+    std::cout << "*******************************************" << std::endl;
+
+    return 0;
+}
+
+bool cmpLine(const string& str1, const string& str2)
+{
+    bool match = true;
+    int i;
+
+    for (i = 0; match && i < str1.length() && i < str2.length(); i++)
+    {
+        match &= str1.at(i) == str2.at(i);
+    }
+
+    if (match)
+    {
+        match = str1.length() == str2.length();
+    }
+
+    return match;
 }
