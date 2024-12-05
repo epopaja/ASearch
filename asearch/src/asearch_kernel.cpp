d5b99d012cadaaefab8940838f361f20d294da60
epopaja <36013260+epopaja@users.noreply.github.com>
2024-12-04 21:37:28 -0600

    new optimize

--- asearch/src/asearch_kernel.cpp
+++ asearch/src/asearch_kernel.cpp
@@ -11,72 +11,58 @@
 
 extern "C"
 {
-    typedef ap_int<32> int32;
-    typedef ap_fixed<32, 16> float32;
-
-    typedef struct {
-        int32 parent_i;
-        int32 parent_j;
-        float32 f, g, h;
-    } cell_t;
-
-    typedef struct {
-        float32 f;
-        int32 i, j;
-    } pPair_t;
-
-    // Optimized A* Search Function
-    void asearch(int32 gridIn[], int32 src_i, int32 src_j, int32 dest_i, int32 dest_j, 
-                 result* res, cell_t cellOut[], int ROW, int COL) {
-
-    #pragma HLS INTERFACE m_axi depth=1024 port=gridIn bundle=gmem
-    #pragma HLS INTERFACE m_axi depth=1024 port=cellOut bundle=gmem
-    #pragma HLS INTERFACE s_axilite port=src_i
-    #pragma HLS INTERFACE s_axilite port=src_j
-    #pragma HLS INTERFACE s_axilite port=dest_i
-    #pragma HLS INTERFACE s_axilite port=dest_j
-    #pragma HLS INTERFACE s_axilite port=res
-    #pragma HLS INTERFACE s_axilite port=ROW
-    #pragma HLS INTERFACE s_axilite port=COL
-    #pragma HLS INTERFACE s_axilite port=return
-
-        // Local grid
-        int32 grid[1024][1024];
-    #pragma HLS ARRAY_PARTITION variable=grid cyclic factor=4 dim=2
-
-        // Load grid into local memory
-        for (int i = 0; i < ROW; i++) {
-    #pragma HLS LOOP_TRIPCOUNT min=1 max=1024
-            for (int j = 0; j < COL; j++) {
-    #pragma HLS PIPELINE II=1
-                grid[i][j] = gridIn[i * COL + j];
+    void asearch(int gridIn[], Pair src, Pair dest, result* res, cell cellOut[]) {
+        // Local grid copy
+        int grid[ROW][COL];
+    #pragma HLS ARRAY_PARTITION variable=grid dim=2 complete
+        for (int x = 0; x < ROW; x++) {
+    #pragma HLS PIPELINE
+            for (int y = 0; y < COL; y++) {
+                grid[x][y] = gridIn[x * COL + y];
             }
         }
 
-        // Check for invalid input or immediate termination cases
-        if (src_i < 0 || src_i >= ROW || src_j < 0 || src_j >= COL ||
-            dest_i < 0 || dest_i >= ROW || dest_j < 0 || dest_j >= COL) {
+        // Local variables
+        result r = PATH_NOT_FOUND;
+
+        // Validation checks
+        if (!isValid(src.first, src.second)) {
             *res = INVALID_SOURCE;
             return;
         }
 
-        // A simple case when the source and destination are the same
-        if (src_i == dest_i && src_j == dest_j) {
+        if (!isValid(dest.first, dest.second)) {
+            *res = INVALID_DESTINATION;
+            return;
+        }
+
+        if (!isUnBlocked(grid, src.first, src.second) ||
+            !isUnBlocked(grid, dest.first, dest.second)) {
+            *res = PATH_IS_BLOCKED;
+            return;
+        }
+
+        if (isDestination(src.first, src.second, dest)) {
             *res = ALREADY_AT_DESTINATION;
             return;
         }
 
-        // Initialization
-        bool closedList[1024][1024] = {false};
-    #pragma HLS ARRAY_PARTITION variable=closedList cyclic factor=4 dim=2
+        // Initialize closed list
+        bool closedList[ROW][COL];
+    #pragma HLS ARRAY_PARTITION variable=closedList dim=2 complete
+        for (int x = 0; x < ROW; x++) {
+    #pragma HLS UNROLL
+            for (int y = 0; y < COL; y++) {
+                closedList[x][y] = false;
+            }
+        }
 
-        cell_t cellDetails[1024][1024];
-    #pragma HLS ARRAY_PARTITION variable=cellDetails cyclic factor=4 dim=2
-
+        // Cell details
+        cell cellDetails[ROW][COL];
+    #pragma HLS ARRAY_PARTITION variable=cellDetails dim=2 complete
         for (int i = 0; i < ROW; i++) {
-    #pragma HLS LOOP_TRIPCOUNT min=1 max=1024
+    #pragma HLS UNROLL
             for (int j = 0; j < COL; j++) {
-    #pragma HLS PIPELINE II=1
                 cellDetails[i][j].f = FLT_MAX;
                 cellDetails[i][j].g = FLT_MAX;
                 cellDetails[i][j].h = FLT_MAX;
@@ -85,51 +71,70 @@ extern "C"
             }
         }
 
-        // Setting up the source
-        int i = src_i, j = src_j;
+        // Set source details
+        int i = src.first, j = src.second;
         cellDetails[i][j].f = 0.0;
         cellDetails[i][j].g = 0.0;
         cellDetails[i][j].h = 0.0;
         cellDetails[i][j].parent_i = i;
         cellDetails[i][j].parent_j = j;
 
-        // Main loop
-        bool foundDest = false;
-        while (!foundDest) {
-            // Check neighbors
-            for (int dir = 0; dir < 8; dir++) {
-    #pragma HLS UNROLL
-                int ni = i + (dir == 0 ? -1 : dir == 1 ? 1 : 0);
-                int nj = j + (dir == 2 ? -1 : dir == 3 ? 1 : 0);
-                if (ni >= 0 && ni < ROW && nj >= 0 && nj < COL &&
-                    !closedList[ni][nj] && grid[ni][nj] != 0) {
-                    float32 g = cellDetails[i][j].g + 1.0;
-                    float32 h = calculateHValue(ni, nj, dest_i, dest_j);  // Implement heuristic function
-                    float32 f = g + h;
-                    if (f < cellDetails[ni][nj].f) {
-                        cellDetails[ni][nj].f = f;
-                        cellDetails[ni][nj].g = g;
-                        cellDetails[ni][nj].h = h;
-                        cellDetails[ni][nj].parent_i = i;
-                        cellDetails[ni][nj].parent_j = j;
+        // Priority queue for open list
+        pPair openList[ROW * COL];
+        int openListSize = 0;
+    #pragma HLS ARRAY_PARTITION variable=openList dim=1 complete
+        addPPair(openList, make_pair(0.0, make_pair(i, j)), &openListSize);
 
-                        if (ni == dest_i && nj == dest_j) {
-                            foundDest = true;
-                            break;
-                        }
+        bool foundDest = false;
+
+        // Main loop
+        while (openListSize > 0 && !foundDest) {
+    #pragma HLS PIPELINE
+            // Extract the node with minimum f value
+            pPair p = openList[0];
+            removePPair(openList, &openListSize);
+
+            i = p.second.first;
+            j = p.second.second;
+            closedList[i][j] = true;
+
+            // Neighbor exploration (8 directions)
+            for (int d = 0; d < 8; d++) {
+    #pragma HLS UNROLL
+                int newI = i + directions[d][0];
+                int newJ = j + directions[d][1];
+                double stepCost = (d < 4) ? 1.0 : 1.414;  // Diagonal cost
+
+                if (isValid(newI, newJ) && !closedList[newI][newJ] &&
+                    isUnBlocked(grid, newI, newJ)) {
+                    if (isDestination(newI, newJ, dest)) {
+                        cellDetails[newI][newJ].parent_i = i;
+                        cellDetails[newI][newJ].parent_j = j;
+                        foundDest = true;
+                        break;
+                    }
+
+                    double newG = cellDetails[i][j].g + stepCost;
+                    double newH = calculateHValue(newI, newJ, dest);
+                    double newF = newG + newH;
+
+                    if (checkF(cellDetails, newI, newJ, newF)) {
+                        addPPair(openList, make_pair(newF, make_pair(newI, newJ)), &openListSize);
+                        cellDetails[newI][newJ].f = newF;
+                        cellDetails[newI][newJ].g = newG;
+                        cellDetails[newI][newJ].h = newH;
+                        cellDetails[newI][newJ].parent_i = i;
+                        cellDetails[newI][newJ].parent_j = j;
                     }
                 }
             }
-            if (foundDest) break;
-            closedList[i][j] = true;  // Mark current as closed
         }
 
-        // Copy cellDetails back to output
-        for (int i = 0; i < ROW; i++) {
-    #pragma HLS LOOP_TRIPCOUNT min=1 max=1024
-            for (int j = 0; j < COL; j++) {
-    #pragma HLS PIPELINE II=1
-                cellOut[i * COL + j] = cellDetails[i][j];
+        // Output results
+        for (int x = 0; x < ROW; x++) {
+    #pragma HLS PIPELINE
+            for (int y = 0; y < COL; y++) {
+                cellOut[x * COL + y] = cellDetails[x][y];
             }
         }
 
